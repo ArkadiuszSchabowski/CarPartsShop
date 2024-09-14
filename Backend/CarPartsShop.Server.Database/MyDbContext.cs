6ac8f083f72141aef6f457733f669bf5406257b8
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-09-14 18:33:35 +0200

    add database, initial configuration

---
+++ Backend/CarPartsShop.Server.Database/MyDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarPartsShop.Server.Database
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
